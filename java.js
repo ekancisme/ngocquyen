c71b8e62f74add95bc8db814a524e860df31e8fd
ekancisme <117744396+ekancisme@users.noreply.github.com>
2023-07-20 12:58:38 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích em vãi <33',
+    btnNo: 'Không bé ơi :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại thích em <3',
+    btnReply: 'Gửi cho em vài lời ngọt ngào đi <3',
+    reply: 'Yêu thì yêu mà không yêu cũng phải núa là yêu <33333333',
+    mess: 'Em biết mà . Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 19 giờ anh qua đón công chúa của anh đi.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/nngocquienn.04' 
+}
